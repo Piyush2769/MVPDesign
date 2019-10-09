d9508488013e36486b39cd83b34c761d691e13d1
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-09 23:01:58 +0530

    Presenter Completed

---
+++ app/src/main/java/com/piyushmaheswari/mvpdesign/Presenter/ILoginPresenter.java
@@ -0,0 +1,5 @@
+package com.piyushmaheswari.mvpdesign.Presenter;
+
+public interface ILoginPresenter {
+    void onLogin(String email,String password);
+}
---
+++ app/src/main/java/com/piyushmaheswari/mvpdesign/View/ILoginView.java
@@ -0,0 +1,5 @@
+package com.piyushmaheswari.mvpdesign.View;
+
+public interface ILoginView {
+    void onLoginResult(String message);
+}
