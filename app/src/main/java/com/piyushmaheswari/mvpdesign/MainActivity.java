ce59e71d438cdb50dcda9e354e67116ed11aa40b
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-09 23:16:20 +0530

    Completed

--- app/src/main/java/com/piyushmaheswari/mvpdesign/MainActivity.java
+++ app/src/main/java/com/piyushmaheswari/mvpdesign/MainActivity.java
@@ -2,12 +2,45 @@ package com.piyushmaheswari.mvpdesign;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.piyushmaheswari.mvpdesign.Presenter.ILoginPresenter;
+import com.piyushmaheswari.mvpdesign.Presenter.LoginPresenter;
+import com.piyushmaheswari.mvpdesign.View.ILoginView;
+
+public class MainActivity extends AppCompatActivity implements ILoginView {
+
+    EditText email,password;
+    Button login;
+
+    ILoginPresenter iLoginPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email=findViewById(R.id.email);
+        password=findViewById(R.id.pass);
+        login=findViewById(R.id.loginBtn);
+
+        iLoginPresenter=new LoginPresenter(this);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iLoginPresenter.onLogin(email.getText().toString(),password.getText().toString());
+            }
+        });
+    }
+
+    @Override
+    public void onLoginResult(String message) {
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
     }
 }
