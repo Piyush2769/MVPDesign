5af21bf36f037ffebd320c2ee31e876d0af91521
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-09 22:59:38 +0530

    Model Completed

--- app/src/main/java/com/piyushmaheswari/mvpdesign/Model/User.java
+++ app/src/main/java/com/piyushmaheswari/mvpdesign/Model/User.java
@@ -1,18 +1,31 @@
 package com.piyushmaheswari.mvpdesign.Model;
 
+import android.text.TextUtils;
+import android.util.Patterns;
+
 public class User implements IUser {
+
+    private String email,password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
     @Override
     public String getEmail() {
-        return null;
+        return email;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public boolean isValidDdata() {
-        return false;
+        return !TextUtils.isEmpty(getEmail()) &&
+                Patterns.EMAIL_ADDRESS.matcher(getEmail()).matches() &&
+                getPassword().length() > 6;
     }
 }
