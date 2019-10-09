1bc07eef4b2871971bd6b4c94589b2c7016a40df
Piyush2769 <startcheckingyourmail@gmail.com>
2019-10-09 22:56:45 +0530

    Initial+1

---
+++ app/src/main/java/com/piyushmaheswari/mvpdesign/Model/IUser.java
@@ -0,0 +1,8 @@
+package com.piyushmaheswari.mvpdesign.Model;
+
+public interface IUser {
+
+    String getEmail();
+    String getPassword();
+    boolean isValidDdata();
+}
---
+++ app/src/main/java/com/piyushmaheswari/mvpdesign/Model/User.java
@@ -0,0 +1,18 @@
+package com.piyushmaheswari.mvpdesign.Model;
+
+public class User implements IUser {
+    @Override
+    public String getEmail() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public boolean isValidDdata() {
+        return false;
+    }
+}
